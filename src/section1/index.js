2c04f11a3584217c94842452651f451daee58e45
anishsingh935 <75081988+anishsingh935@users.noreply.github.com>
2023-05-14 01:09:50 +0530

    update

--- src/section1/index.js
+++ src/section1/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import {increaseValue, decreaseValue} from "../Action/index";
+import {  useSelector } from 'react-redux';
 
 function Index() {
     const value = useSelector(state => state.value);
-    const dispatch = useDispatch();
     return (
         <div>
             <div>Section {value} </div>
